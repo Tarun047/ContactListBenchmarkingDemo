e1206e6f5554726618d7247e6c4a0819b78db6ef
gtarun047@gmail.com <gtarun047@gmail.com>
2024-03-10 15:20:53 +0530

    Update Benchmark

--- ContactBook.Benchmark/Features/ContactBookFeatureBenchmark.cs
+++ ContactBook.Benchmark/Features/ContactBookFeatureBenchmark.cs
@@ -11,11 +11,11 @@ public class ContactBookFeatureBenchmark : ContactBookDataGenBase
     [Benchmark]
     public void BenchmarkSearch()
     {
-        var contactSearcher = new Bussiness.ContactBook(Contacts);
+        var contactBook = new Bussiness.ContactBook(Contacts);
         var matchedContacts = new List<Contact>();
         foreach (var name in NamesToSearch)
         {
-            matchedContacts.AddRange(contactSearcher.SearchByName(name));
+            matchedContacts.AddRange(contactBook.SearchByName(name));
         }
     }
 }
